1861bf4dd8d91e9f689752e10992863aba979859
Nitin Sali <nadeem+NitinSali@ytel.com>
2015-12-07 08:36:18 +0530

    files for calls,email,sms

--- v1b/examples/Calls/create-call.php
+++ v1b/examples/Calls/create-call.php
@@ -4,39 +4,40 @@ require_once '../../library/message360.php';
 # Now what we need to do is instantiate the library and set the required options defined above
 $Message360 = Message360::getInstance();
 # Message360 REST API credentials are required
-$Message360 -> setOptions(array( 
-   'account_sid'       => 'xxxxxxx', 
-   'auth_token'        => 'xxxxxxx',    
-   'response_to_array' => true,
+$Message360 -> setOptions(array(
+'account_sid' => 'xxxxxxx',
+'auth_token' => 'xxxxxxx',
+'response_to_array' => true,
 ));
-try 
+try
 {
 # Make Call
 $call = $Message360->create('Calls','makeCall', array(
-        'To' => 'XXXXXXXXXX',//required
-        'From'   => 'XXXXXXXXXX', //required
-        'Url'  => 'XXXXXX', //required
-        'Method'  => 'XXXXXX', //optional
-        'StatusCallback'  => 'XXXXXX', //optional
-        'StatusCallbackMethod'  => 'XXXXXX', //optional
-        'FallbackUrl'  => 'XXXXXX', //optional
-        'FallbackMethod'  => 'XXXXXX', //optional
-        'HeartbeatUrl'  => 'XXXXXX', //optional
-        'HeartbeatMethod'  => 'XXXXXX', //optional
-        'ForwardedFrom'  => 'XXXXXX', //optional
-        'Timeout'  => 'XXXXXX', //optional
-        'PlayDtmf'  => 'XXXXXX', //optional
-        'HideCallerId'  => 'XXXXXX', //optional
-        'Record'  => 'XXXXXX', //optional
-        'RecordCallback'  => 'XXXXXX', //optional
-        'RecordCallbackMethod'  => 'XXXXXX', //optional
-        'Transcribe'  => 'XXXXXX',//optional
-        'TranscribeCallback'  => 'XXXXXX', //optional
-    ));
+'ToCountryCode' => 1, //required
+'To' => 'XXXXXXXXXX',//required
+'FromCountryCode' => 1, //required
+'From' => 'XXXXXXXXXX', //required
+'Url' => 'XXXXXX', //required
+'Method' => 'XXXXXX', //optional
+'StatusCallback' => 'XXXXXX', //optional
+'StatusCallbackMethod' => 'XXXXXX', //optional
+'FallbackUrl' => 'XXXXXX', //optional
+'FallbackMethod' => 'XXXXXX', //optional
+'HeartbeatUrl' => 'XXXXXX', //optional
+'HeartbeatMethod' => 'XXXXXX', //optional
+'ForwardedFrom' => 'XXXXXX', //optional
+'Timeout' => 'XXXXXX', //optional
+'PlayDtmf' => 'XXXXXX', //optional
+'HideCallerId' => 'XXXXXX', //optional
+'Record' => 'XXXXXX', //optional
+'RecordCallback' => 'XXXXXX', //optional
+'RecordCallbackMethod' => 'XXXXXX', //optional
+'Transcribe' => 'XXXXXX',//optional
+'TranscribeCallback' => 'XXXXXX', //optional
+));
 print_r($call->getResponse());
-} 
-catch (Message360_Exception $e) 
-{
-   echo "Error occured: " . $e->getMessage() . "\n";   
 }
-
+catch (Message360_Exception $e)
+{
+echo "Error occured: " . $e->getMessage() . "\n";
+}
--- v1b/examples/SMS/number-optin.php
+++ v1b/examples/SMS/number-optin.php
@@ -3,29 +3,28 @@
 require_once '../../library/message360.php';
 // Now what we need to do is instantiate the library and set the required options defined above
 $Message360 = Message360::getInstance();
-// Message360 REST API credentials are required 
-$Message360 -> setOptions(array( 
-    'account_sid'       => 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX', 
-    'auth_token'        => 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
-    'response_to_array' =>true,
+// Message360 REST API credentials are required
+$Message360 -> setOptions(array(
+'account_sid' => 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+'auth_token' => 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+'response_to_array' =>true,
 ));
-try 
+try
 {
-    //Opt-In a Number Parameters
-    $optInNumber = $Message360->listAll('sms','numberoptin',array(
-	    'FromCountryCode'   => 1, //required
-		'From' => 'XXXXXXXXXX', //required           
-		'ToCountryCode'  => 1, //required
-		'To' => 'XXXXXXXXXX', //required
-	    'Expires'=> 0, //required
-	    'AuthorizedBy'=> 'Self', //required
-	    'AuthorizedHow'=> 'Self Added' //required
-	));
-    // Print content of the $getInboundSMS objects
-    print_r($optInNumber->getResponse()); exit;
-} 
-catch (Message360_Exception $e) 
+//Opt-In a Number Parameters
+$optInNumber = $Message360->listAll('sms','numberoptin',array(
+'From' => 'XXXXXXXXXX', //required
+'To' => 'XXXXXXXXXX', //required
+'Expires'=> 0, //required
+'AuthorizedBy'=> 'Self', //required
+'AuthorizedHow'=> 'Self Added' //required
+));
+
+// Print content of the $getInboundSMS objects
+print_r($optInNumber->getResponse()); exit;
+}
+catch (Message360_Exception $e)
 {
-  echo "Error occured: " . $e->getMessage() . "\n";
+echo "Error occured: " . $e->getMessage() . "\n";
 }
 ?>
