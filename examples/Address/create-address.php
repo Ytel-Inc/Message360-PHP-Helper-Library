818d44430e6a9f4824bb6551870253a8bf17c554
Rohtas Gupta <rohtas@ytel.co.in>
2016-12-28 12:49:40 +0530

    Address Create api helper

--- examples/Address/create-address.php
+++ examples/Address/create-address.php
@@ -7,22 +7,23 @@ $Message360 = Message360API\Lib\Message360::getInstance();
 
 // Message360 REST API credentials are required
 $Message360 -> setOptions(array( 
-    'account_sid'       => 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX', 
-    'auth_token'        => 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+    'account_sid'       => 'cbc6a0b5-c113-a6e4-4f96-53366a7c9966', 
+    'auth_token'        => '5964d5de074084894ff57f7771b296c3',
     'response_to_array' =>true,
 ));
 try 
 {
     // Fetch Send SMS
     $sendSMS = $Message360->create('Address','createAddress', array(
-            'FromCountryCode'   => 1, //required
-            'From' => 'XXXXXXXXXX', //required             
-            'ToCountryCode'  => 1, //required
-            'To' => 'XXXXXXXXXX', //required
-            'Body' => 'Sms Text', //required
-            'SmartSms' => 'false',//Ex.true or false  //optional 
-            'Method' => 'POST',//Ex.POST or GET  //optional 
-            'MessageStatusCallback' => 'Message Status callback URL' //optional
+            'name'   => 'XXXXXXXXXXXXXXXXXXXX', //required
+            'address' => 'XXXXXXXXXXXXXXXXXXX', //required             
+            'state'  => 'XX',                   //required
+            'city' => 'XXXXXXXXXXXXXX',//required
+            'country' => 'US',                  //required
+            'zip' => '94107',                   //required 
+            'Phone' => '1234567890',          //optional 
+            'Email' => 'XXXXXXXX@XXXXXXX.XX',   //optional 
+            'Description' => 'XXXXXXXXXXXXXXXX' //optional
     ));
     // Print content of the $sendSMS objects
     print_r($sendSMS->getResponse());
