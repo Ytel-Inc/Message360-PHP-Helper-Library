d054a569776bb776080fe9276f5198559d7ad89e
Rohtas Gupta <rohtas@ytel.co.in>
2016-12-28 11:57:20 +0530

    List address implemented

--- examples/Address/list-address.php
+++ examples/Address/list-address.php
@@ -5,23 +5,21 @@ require_once '../../library/message360.php';
 $Message360 = Message360API\Lib\Message360::getInstance();
 // Message360 REST API credentials are required
 $Message360 -> setOptions(array( 
-    'account_sid'       => 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX', 
-    'auth_token'        => 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+    'account_sid'       => 'cbc6a0b5-c113-a6e4-4f96-53366a7c9966', 
+    'auth_token'        => '5964d5de074084894ff57f7771b296c3',
     'response_to_array' =>true,
 ));
 try {
-    // Fetch listSMS
-    $listSMS = $Message360->listAll('Address','listAddress',array(
+    // Fetch listAddress
+    $listAddresses = $Message360->listAll('Address','listAddress',array(
         'Page'=> 1 , //optional
         'PageSize'=>10, //optional
-        'DateCreated'=>'XXXX-XX-XX', //optional
-        'From'=>'XXXXXXXXXX', //optional
-        'To'=>'XXXXXXXXXX', //optional
+        'DateCreated'=>'2016-11-14', //optional
     ));
-    // Print content of the $listSMS objects
-    foreach($listSMS->getResponse() as $listsSMS) 
+    // Print content of the $listAddress objects
+    foreach($listAddresses->getResponse() as $listAddress) 
     {
-        print_r($listsSMS);
+        print_r($listAddress);
     }
 } 
 catch (Message360_Exception $e) 
